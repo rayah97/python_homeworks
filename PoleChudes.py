1fd370c8189d816e78af17cdfa2b3aab7f2faa21
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-25 18:12:50 +0400

    added 2 homeworks

--- PoleChudes.py
+++ PoleChudes.py
@@ -1,17 +1,16 @@
 selected_country = "France"
 selected_capital = "Paris"
 hidden_capital = "*" * len(selected_capital)
-guessed_letters = []
 
 while "*" in hidden_capital:
     letter = input("Guess a letter in the capital of " + selected_country + ": ").lower()
-    if letter in guessed_letters:
-        print("You already guessed that letter.")
-    elif letter in selected_capital.lower():
-        hidden_capital = "".join([selected_capital[i] if selected_capital[i].lower() == letter else hidden_capital[i] for i in range(len(selected_capital))])
+
+    if letter in selected_capital.lower():
+        for i in range(len(selected_capital)):
+            if selected_capital[i].lower() == letter and hidden_capital[i] == "*":
+                hidden_capital = hidden_capital[:i] + selected_capital[i] + hidden_capital[i+1:]
         print("Correct! The word is now: " + hidden_capital)
     else:
         print("Incorrect.")
-    guessed_letters.append(letter)
 
 print("Congratulations! The capital of " + selected_country + " is " + selected_capital + ".")
